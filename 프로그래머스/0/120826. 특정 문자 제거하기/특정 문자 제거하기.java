e811fc62a148e51a3a04739ac73d0b6ea28caae2
koo sun kyung <121647187+qunsk@users.noreply.github.com>
2024-07-04 22:39:28 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        String answer = my_string.replace(String.valueOf(letter), "");
+        String answer = my_string.replace(letter, "");
         return answer;
     }
 }

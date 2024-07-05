0d87d9976d531ddde6821ba5ad84880c97207a6e
koo sun kyung <121647187+qunsk@users.noreply.github.com>
2024-07-05 17:31:57 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.reverse();
+        return sb.toString();
+    }
+}

160d4b4c417c91895c9471f76bdbe3f172467625
koo sun kyung <121647187+qunsk@users.noreply.github.com>
2024-08-02 01:51:00 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,12 +1,8 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int width = 0;
-        int len = 0;
-        int height = 0;
-        
-        width = box[0]/n;
-        len = box[1]/n;
-        height = box[2]/n;
+        int width = box[0]/n;
+        int len = box[1]/n;
+        int height = box[2]/n;
         
         return width*len*height;
     }

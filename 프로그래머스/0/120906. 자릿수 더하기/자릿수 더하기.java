c266774c504bb6049b17e82c7093c22f4b156475
koo sun kyung <121647187+qunsk@users.noreply.github.com>
2024-09-06 12:36:40 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(true){
+            answer += n%10;
+            n/=10;
+            if(n == 0) break;
+        }
+        return answer;
+    }
+}
